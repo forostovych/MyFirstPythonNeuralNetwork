4daaca9adf41ff8110a758917baacb11c9d16ee1
Valera Frost <valera.viyar@gmail.com>
2024-08-17 23:25:25 +0300

    add augmentation

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Импортируем модули
 import torch
 import torch.nn as nn
 import torchvision
@@ -107,3 +108,55 @@ if __name__ == '__main__':
             correct += (predicted == labels).sum().item()
 
     print(f'Точность на тестовых данных: {100 * correct / total}%')
+
+    # Подготавливаем данные - аугментируем данные
+    transform_augmented = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    trainset_augmented = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                                      download=True, transform=transform_augmented)
+    trainloader_augmented = torch.utils.data.DataLoader(trainset_augmented, batch_size=32,
+                                                        shuffle=True, num_workers=2)
+
+    # Дообучаем модель на аугментированных данных
+    for epoch in range(4):  # количество эпох для дообучения
+        running_loss = 0.0
+        epoch_start_time = time.time()
+        print(Fore.GREEN + "Эпоха (дообучение): " + str(epoch) + Style.RESET_ALL)
+        for i, data in enumerate(trainloader_augmented, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print(f'[Epoch (дообучение) {epoch + 1}, Batch {i + 1}] loss: {running_loss / 2000:.3f}')
+                running_loss = 0.0
+
+        epoch_end_time = time.time()
+        epoch_time = epoch_end_time - epoch_start_time
+        print(Fore.CYAN + f"Эпоха (дообучение) {epoch + 1} завершена за {epoch_time:.3f} секунд" + Style.RESET_ALL)
+
+    # Тестируем модель после дообучения
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(Fore.MAGENTA + f'Точность на тестовых данных после дообучения: {100 * correct / total}%' + Style.RESET_ALL)
